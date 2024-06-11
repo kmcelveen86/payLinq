6dc60eae41ee68956f76eba74bb3951f166bdb52
gdavis <gregory.davis@autozone.com>
2024-06-11 10:59:11 -0500

    feat(auth): set up sign and and register forms

---
+++ components/SignUp/index.tsx
@@ -0,0 +1,171 @@
+"use client";
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import AppleIcon from "@mui/icons-material/Apple";
+import Google from "@mui/icons-material/Google";
+import { useRouter } from "next/navigation";
+
+const theme = createTheme();
+
+export default function SignUp() {
+  const router = useRouter();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      firstName: data.get("firstName"),
+      lastName: data.get("lastName"),
+      email: data.get("email"),
+      password: data.get("password"),
+      confirmPassword: data.get("confirmPassword"),
+    });
+  };
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  const handleGoogleSignUp = () => {
+    // Implement Google sign-up logic here
+    console.log("Google sign-up");
+  };
+
+  const handleAppleSignUp = () => {
+    // Implement Apple sign-up logic here
+    console.log("Apple sign-up");
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type="password"
+                  id="confirmPassword"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}>
+              Sign Up
+            </Button>
+            <Typography variant="body2" sx={{ mt: 2 }}>
+              Or,
+            </Typography>
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 2, mb: 1, bgcolor: "#DB4437", color: "white" }}
+              onClick={handleGoogleSignUp}
+              startIcon={<Google />}>
+              Sign up with Google
+            </Button>
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mb: 2, bgcolor: "black", color: "white" }}
+              onClick={handleAppleSignUp}
+              startIcon={<AppleIcon />}>
+              Sign up with Apple
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/signin" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+          <Button
+            startIcon={<ArrowBackIcon />}
+            onClick={handleGoBack}
+            sx={{ mt: 2 }}>
+            Go Back
+          </Button>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
