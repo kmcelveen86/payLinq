b66352f7a6e6e8ac962533f7657f20ab512380f0
Gregory Davis <gdavis@gregm4.local>
2025-03-21 03:35:35 -0500

    chore(deps): update more deps

--- components/SignInInput/index.tsx
+++ components/SignInInput/index.tsx
@@ -3,7 +3,6 @@ import { cookies } from "next/headers";
 
 export default async function SignInInput() {
   const csrfToken = (await cookies()).get("next-auth.csrf-token")?.value.split("|")[0];
-  console.log('GREG LOOK!  ~ SignInInput ~ csrfToken:', csrfToken);
 
   return <input name="csrfToken" type="hidden" defaultValue={csrfToken} />;
 }
